7606e332de40661c64926f87ca7d54ca02c93c58
andystaples <77818326+andystaples@users.noreply.github.com>
2024-06-20 14:05:52 -0600

    Add 1ES compliant pipeline (#19)

--- build.ps1
+++ build.ps1
@@ -10,95 +10,87 @@ param(
     [string]
     $Configuration = 'Debug',
     [switch]
-    $AddSBOM
+    $NoBuild,
+    [switch]
+    $Test
 )
 
 Import-Module "$PSScriptRoot\pipelineUtilities.psm1" -Force
 
-$packageName = "AzureFunctions.PowerShell.OpenTelemetry.SDK"
-$shimPath = "$PSScriptRoot/src/AzureFunctions.PowerShell.OpenTelemetry.SDK"
-$otelEnginePath = "$PSScriptRoot/src/OpenTelemetryEngine"
-$otelAppPath = "$PSScriptRoot/test/E2E/app/Modules/$packageName"
-$powerShellModulePath = "$PSScriptRoot/src/$packageName.psm1"
-$manifestPath = "$PSScriptRoot/src/$packageName.psd1"
+if (!$NoBuild.IsPresent) {
 
-# Publish to /out/ folder
-# When test app added, publish there instead
-$outputPath = $otelAppPath
-
-$sharedDependenciesPath = "$outputPath/Dependencies/"
-
-$netCoreTFM = 'net6.0'
-$publishPathSuffix = "bin/$Configuration/$netCoreTFM/publish"
-
-#region BUILD ARTIFACTS ===========================================================================
-Write-Log "Build started..."
-Write-Log "Configuration: '$Configuration'`nOutput folder '$outputPath'`nShared dependencies folder: '$sharedDependenciesPath'" "Gray"
-
-# Map from project names to the folder containing the corresponding .csproj
-$projects = @{
-    'OpenTelemetry SDK' = $shimPath
-    'OpenTelemetry Engine' = $otelEnginePath
-}
-
-# Remove previous build if it exists
-Write-Log "Removing previous build from $outputPath if it exists..." "Cyan"
-if (Test-Path $outputPath)
-{
-    Remove-Item -Path $outputPath -Recurse -Force -ErrorAction Ignore
-}
-# Create output folder and its inner dependencies directory
-Write-Log "Creating a new output and shared dependencies folder at $outputPath and $sharedDependenciesPath..." "Cyan"
-[void](New-Item -Path $sharedDependenciesPath -ItemType Directory)
-
-# Build the OTel SDK project
-foreach ($project in $projects.GetEnumerator()) {
-    Write-Log "Building $($project.Name) project with target framework $netCoreTFM...."
-    Push-Location $project.Value
-    try
-    {
-        dotnet publish -f $netCoreTFM -c $Configuration
-    }
-    finally
-    {
-        Pop-Location
-    }
-}
-
-$commonFiles = [System.Collections.Generic.HashSet[string]]::new()
-
-Write-Log "Copying assemblies from the OpenTelemetry Engine project into $sharedDependenciesPath" "Gray"
-Get-ChildItem -Path (Join-Path "$otelEnginePath" "$publishPathSuffix") |
-    Where-Object { $_.Extension -in '.dll','.pdb' } |
-    ForEach-Object { [void]$commonFiles.Add($_.Name); Copy-Item -LiteralPath $_.FullName -Destination $sharedDependenciesPath }
-
-# Copy all *unique* assemblies from OTel SDK into output directory
-Write-Log "Copying unique assemblies from the OTel SDK project into $outputPath" "Gray"
-Get-ChildItem -Path (Join-Path "$shimPath" "$publishPathSuffix") |
-    Where-Object { $_.Extension -in '.dll','.pdb' -and -not $commonFiles.Contains($_.Name) } |
-    ForEach-Object { Copy-Item -LiteralPath $_.FullName -Destination $outputPath }
-
-# Move OTel SDK manifest into the output directory
-Write-Log "Copying PowerShell module and manifest from the OTel SDK source code into $outputPath" "Gray"
-Copy-Item -Path $powerShellModulePath -Destination $outputPath
-Copy-Item -Path $manifestPath -Destination $outputPath
-Write-Log "Build succeeded!"
-#endregion
-
-#region ADD SBOM ==================================================================================
-if ($AddSBOM) {
-    # Install manifest tool
-    $manifestToolPath = Install-SBOMUtil
-    Write-Log "Manifest tool path: $manifestToolPath"
-
-    # Generate manifest
-    $telemetryFilePath = Join-Path $PSScriptRoot ((New-Guid).Guid + ".json")
     $packageName = "AzureFunctions.PowerShell.OpenTelemetry.SDK"
+    $shimPath = "$PSScriptRoot/src/AzureFunctions.PowerShell.OpenTelemetry.SDK"
+    $otelEnginePath = "$PSScriptRoot/src/OpenTelemetryEngine"
+    $otelAppPath = "$PSScriptRoot/test/E2E/app/Modules/$packageName"
+    $powerShellModulePath = "$PSScriptRoot/src/$packageName.psm1"
+    $manifestPath = "$PSScriptRoot/src/$packageName.psd1"
 
-    Write-Log "Running: dotnet $manifestToolPath generate -BuildDropPath $outputPath -BuildComponentPath $outputPath -Verbosity Information -t $telemetryFilePath -PackageName $packageName"
-    dotnet $manifestToolPath generate -BuildDropPath $outputPath -BuildComponentPath $outputPath -Verbosity Information -t $telemetryFilePath -PackageName $packageName
+    # Publish to /out/ folder
+    # When test app added, publish there instead
+    $outputPath = $otelAppPath
 
-    # Discard telemetry generated
-    Remove-Item -Path $telemetryFilePath -ErrorAction Ignore
+    $sharedDependenciesPath = "$outputPath/Dependencies/"
+
+    $netCoreTFM = 'net6.0'
+    $publishPathSuffix = "bin/$Configuration/$netCoreTFM/publish"
+
+    #region BUILD ARTIFACTS ===========================================================================
+    Write-Log "Build started..."
+    Write-Log "Configuration: '$Configuration'`nOutput folder '$outputPath'`nShared dependencies folder: '$sharedDependenciesPath'" "Gray"
+
+    # Map from project names to the folder containing the corresponding .csproj
+    $projects = @{
+        'OpenTelemetry SDK' = $shimPath
+        'OpenTelemetry Engine' = $otelEnginePath
+    }
+
+    # Remove previous build if it exists
+    Write-Log "Removing previous build from $outputPath if it exists..." "Cyan"
+    if (Test-Path $outputPath)
+    {
+        Remove-Item -Path $outputPath -Recurse -Force -ErrorAction Ignore
+    }
+    # Create output folder and its inner dependencies directory
+    Write-Log "Creating a new output and shared dependencies folder at $outputPath and $sharedDependenciesPath..." "Cyan"
+    [void](New-Item -Path $sharedDependenciesPath -ItemType Directory)
+
+    # Build the OTel SDK project
+    foreach ($project in $projects.GetEnumerator()) {
+        Write-Log "Building $($project.Name) project with target framework $netCoreTFM...."
+        Push-Location $project.Value
+        try
+        {
+            dotnet publish -f $netCoreTFM -c $Configuration
+        }
+        finally
+        {
+            Pop-Location
+        }
+    }
+
+    $commonFiles = [System.Collections.Generic.HashSet[string]]::new()
+
+    Write-Log "Copying assemblies from the OpenTelemetry Engine project into $sharedDependenciesPath" "Gray"
+    Get-ChildItem -Path (Join-Path "$otelEnginePath" "$publishPathSuffix") |
+        Where-Object { $_.Extension -in '.dll','.pdb' } |
+        ForEach-Object { [void]$commonFiles.Add($_.Name); Copy-Item -LiteralPath $_.FullName -Destination $sharedDependenciesPath }
+
+    # Copy all *unique* assemblies from OTel SDK into output directory
+    Write-Log "Copying unique assemblies from the OTel SDK project into $outputPath" "Gray"
+    Get-ChildItem -Path (Join-Path "$shimPath" "$publishPathSuffix") |
+        Where-Object { $_.Extension -in '.dll','.pdb' -and -not $commonFiles.Contains($_.Name) } |
+        ForEach-Object { Copy-Item -LiteralPath $_.FullName -Destination $outputPath }
+
+    # Move OTel SDK manifest into the output directory
+    Write-Log "Copying PowerShell module and manifest from the OTel SDK source code into $outputPath" "Gray"
+    Copy-Item -Path $powerShellModulePath -Destination $outputPath
+    Copy-Item -Path $manifestPath -Destination $outputPath
+    Write-Log "Build succeeded!"
+    #endregion
+}
+#region Test ==================================================================================
+if ($Test.IsPresent) {
+    Write-Log "There are no tests! Panic!"
 }
 #endregion
